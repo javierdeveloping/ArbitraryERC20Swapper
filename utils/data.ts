d77d1553e6caec65080d9bc9ecbe12c794e280de
Javier <javierdrab@gmail.com>
2024-06-06 14:07:54 +0200

    deployment data and copy sepolia data to local data for testing purposes
    Deployment data for sepolia network

--- utils/data.ts
+++ utils/data.ts
@@ -13,8 +13,8 @@ export const uniswapData: UniswapData = {
   },
   //mock addresses
   local: {
-    swapRouter02: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
-    quoterV2: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
+    swapRouter02: "0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E",
+    quoterV2: "0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3",
   },
 };
 
@@ -40,12 +40,12 @@ export const coinData: CoinData = {
     DAI: {
       name: "DAI",
       decimals: 18,
-      address: "",
+      address: "0x68194a729C2450ad26072b3D33ADaCbcef39D574",
     },
     USDT: {
       name: "Tether USDT Stablecoin",
       decimals: 6,
-      address: "",
+      address: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0",
     },
     WETH: {
       name: "Wrapped ETH",
@@ -57,17 +57,17 @@ export const coinData: CoinData = {
     DAI: {
       name: "DAI",
       decimals: 18,
-      address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      address: "0x68194a729C2450ad26072b3D33ADaCbcef39D574",
     },
     USDT: {
       name: "Tether USDT Stablecoin",
       decimals: 6,
-      address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+      address: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0",
     },
     WETH: {
       name: "Wrapped ETH",
       decimals: 18,
-      address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      address: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
     },
   },
 };
